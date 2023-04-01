1a700be07c4e7fed6211caae4432bc25c6ce818c
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-04-01 17:26:20 +0200

    added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoListApp';
+  title: string = 'strange hobby';
+  discription: string = 'My strange hobby may be that even though I am studying to become a software developer, I read a lot of medical literature, watch a lot of medical content and am fascinated by observation during surgery. I collect medical instruments and literature. I was present at heavy dressings with open bones, and also assisted in suturing soft tissues with severe lacerations. I think this is a rather strange hobby.';
+  isWhite: boolean = false;
+  isBlack: boolean = true;
+  photosUrl: string[] = [
+    '../assets/med.jpg',
+    '../assets/med2.webp'
+  ]
 }
